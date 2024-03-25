3953de612a1a5dd9d1f41991525e4add918d57a7
Adrian <ooikeanhong168@gmail.com>
2024-03-25 16:25:17 +0800

    excel file handler

--- src/components/excel.tsx
+++ src/components/excel.tsx
@@ -9,8 +9,6 @@ import { getExcelFile } from "../services/read";
 //   name: string;
 // }
 
-
-
 const UploadExcelPage = () => {
   const [file, setFile] = useState<File | null>(null); // Added type File for useState
   // const [fileNames, setFileNames] = useState<FileItem[]>([]);
@@ -25,6 +23,21 @@ const UploadExcelPage = () => {
           severity: "error",
           summary: "Upload failed",
           detail: "Please choose a file",
+        });
+      }
+      return;
+    }
+
+    // Check if the file extension is xlsx
+    const fileNameParts = file.name.split(".");
+    const fileExtension = fileNameParts[fileNameParts.length - 1];
+    if (fileExtension !== "xlsx") {
+      console.error("Invalid file format");
+      if (toastRef.current != null) {
+        toastRef.current.show({
+          severity: "error",
+          summary: "Upload failed",
+          detail: "Please choose an Excel file (xlsx)",
         });
       }
       return;
@@ -57,7 +70,7 @@ const UploadExcelPage = () => {
     getExcelFile(setFileNames); // Pass setFileNames directly
   }, []);
 
-  // console.log(fileNames); 
+  // console.log(fileNames);
 
   return (
     <div className="updateBox">
@@ -65,9 +78,12 @@ const UploadExcelPage = () => {
       <Card title="File" className="md:w-45rem" style={{ width: "33em" }}>
         <div className="file-upload-container">
           <label htmlFor="file-upload" className="custom-file-upload">
-            Choose file
+            Choose File
           </label>
-          <p>{file ? file.name : ""} </p>
+          <div className="file-name-container">
+            <p className="file-name">{file ? file.name : ""}</p>
+          </div>
+          {/* {file && <p style={{ margin: 0 }}>{file.name}</p>} */}
         </div>
         <input
           id="file-upload"
@@ -75,13 +91,19 @@ const UploadExcelPage = () => {
           accept=".xls,.xlsx"
           onChange={(e) => setFile(e.target.files ? e.target.files[0] : null)}
         />
-        <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            marginTop: "1.5em",
+          }}
+        >
           <Button
             type="button"
             onClick={upload}
             icon="pi pi-upload"
             label="Upload"
-            style={{ marginTop: "0.5em" }}
+            style={{ width: "8em", marginRight: "0.5em" }}
           />
           <Button
             type="button"
@@ -89,14 +111,17 @@ const UploadExcelPage = () => {
             icon="pi pi-trash"
             severity="danger"
             label="Clear"
-            style={{ marginLeft: "1em" }}
+            style={{ width: "8em" }}
           />
         </div>
       </Card>
+
       <div>
         {fileNames.map((val, key) => {
           return (
-            <div key={key}> {/* Add key prop for each mapped item */}
+            <div key={key}>
+              {" "}
+              {/* Add key prop for each mapped item */}
               <h3>{val}</h3> {/* Access the file name directly */}
             </div>
           );
