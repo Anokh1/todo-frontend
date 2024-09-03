6d50cc0b3b2a1feeb69609bdbe7a990310965012
Adrian <ooikeanhong168@gmail.com>
2024-09-03 16:34:36 +0800

    Update: fix user context

--- src/utilities/Context/UserContext.tsx
+++ src/utilities/Context/UserContext.tsx
@@ -1,7 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import UserService from "services/user.service";
 import { useLoading } from "./LoadingContext";
+import { getToken } from "utilities/Function/getToken.function";
+import { callApi } from "utilities/Function/callApi.function";
+import config from "config/server.config";
+import { ClearToken } from "utilities/Function/clearToken.function";
 
 export type UserContextProps = {
   user: {
@@ -32,4 +36,46 @@ export const UserContextProvider = ({ children }: any) => {
   const { startLoading, stopLoading } = useLoading();
 
   const [user, setUser] = useState<any>(null);
+
+  const navigateToLogin = () => {
+    window.location.href = `${config.hostname}:${config.backend_port}/login`;
+  };
+
+  useEffect(() => {
+    if (getToken()) {
+      let apiFunction = userService.getOneUser;
+      startLoading();
+
+      callApi({
+        apiFunction,
+        setLoading,
+        navigateToLogin: () => {
+          navigateToLogin();
+        },
+      }).then((res: any) => {
+        if (res.status) {
+          if (!res.data) {
+            ClearToken();
+            navigateToLogin();
+            return;
+          }
+          if (res.data.isActive === 0) {
+            ClearToken();
+            navigateToLogin();
+            return;
+          }
+          setUser(res.data);
+        }
+      });
+      stopLoading();
+    } else {
+      navigateToLogin();
+    }
+  }, [getToken()]);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
