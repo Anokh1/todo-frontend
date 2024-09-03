2169065547c7d78b4fb5f3a45126e6191d9324d7
Adrian <ooikeanhong168@gmail.com>
2024-09-03 14:36:19 +0800

    Update: add server config

---
+++ src/config/server.config.tsx
@@ -0,0 +1,19 @@
+interface configProps {
+  hostname: string;
+  frontend_port: number;
+  backend_port: number;
+}
+console.log("REACT ENV", import.meta.env.VITE_APP_ENV);
+
+const devConfig: configProps = {
+  hostname: "http://localhost",
+  frontend_port: 3112,
+  backend_port: 3002,
+};
+
+let config: configProps;
+let loginConfig: configProps;
+
+config = devConfig;
+
+export { config as default, loginConfig };
